05938f18cc9eae0ca27612470607708e3d5ca5c1
Daniel Garcia Vizcaino <danielgarciavizcaino@gmail.com>
2022-09-02 22:26:39 +0200

    Finish the 3 initial functions

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 function playRock(){
     let computerChoice = Math.floor(Math.random()*3);
+    let answer = document.querySelector("#answer");
     if(computerChoice === 0){
         console.log("You won! You chose rock and the computer chose scissors");
+        answer.innerHTML = "You won! You chose rock and the computer chose scissors";
     }else if(computerChoice === 1){
         console.log("It's a tie, you both chose rock");
+        answer.innerHTML = "It's a tie, you both chose rock";
     }else if(computerChoice === 2){
         console.log("You lost... You chose rock and the computer chose paper");
+        answer.innerHTML = "You lost... You chose rock and the computer chose paper";
     }
 }
 
@@ -15,12 +19,16 @@ rockbutton.addEventListener("click", playRock);
 
 function playPaper(){
     let computerChoice = Math.floor(Math.random()*3);
+    let answer = document.querySelector("#answer");
     if(computerChoice === 0){
         console.log("You won! You chose paper and the computer chose rock");
+        answer.innerHTML = "You won! You chose paper and the computer chose rock";
     }else if(computerChoice === 1){
-        console.log("It's a tie, you both chose rock");
+        console.log("It's a tie, you both chose paper");
+        answer.innerHTML = "It's a tie, you both chose rock";
     }else if(computerChoice === 2){
         console.log("You lost... You chose paper and the computer chose scissors");
+        answer.innerHTML = "You lost... You chose paper and the computer chose scissors";
     }
 }
 
@@ -30,12 +38,16 @@ paperbutton.addEventListener("click", playPaper);
 
 function playScissors(){
     let computerChoice = Math.floor(Math.random()*3);
+    let answer = document.querySelector("#answer");
     if(computerChoice === 0){
         console.log("You won! You chose scissors and the computer chose paper");
+        answer.innerHTML = "You won! You chose scissors and the computer chose paper";
     }else if(computerChoice === 1){
         console.log("It's a tie, you both chose rock");
+        answer.innerHTML = "It's a tie, you both chose scissors";
     }else if(computerChoice === 2){
         console.log("You lost... You chose scissors and the computer chose rock");
+        answer.innerHTML = "You lost... You chose scissors and the computer chose rock";
     }
 }
 
