6f0953eb49b69813c48127b2ae02c471dd0d8663
Daniel Garcia Vizcaino <danielgarciavizcaino@gmail.com>
2022-09-03 13:52:50 +0200

    Make code more readable

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
-let playerscore = 0;
-let computerscore = 0;
-const yourscoretext = document.querySelector("#yourscore");
-const computerscoretext = document.querySelector("#computerscore");
+let playerScore = 0;
+let computerScore = 0;
+const yourScoreText = document.querySelector("#yourScore");
+const computerScoreText = document.querySelector("#computerScore");
 
 function playRock(){
     let computerChoice = Math.floor(Math.random()*3);
     let answer = document.querySelector("#answer");
     if(computerChoice === 0){
-        playerscore += 1;
-        yourscoretext.innerHTML = playerscore;
+        playerScore += 1;
+        yourScoreText.innerHTML = playerScore;
         console.log("You won! You chose rock and the computer chose scissors");
         answer.innerHTML = "You won! You chose rock and the computer chose scissors";
     }else if(computerChoice === 1){
         console.log("It's a tie, you both chose rock");
         answer.innerHTML = "It's a tie, you both chose rock";
     }else if(computerChoice === 2){
-        computerscore += 1;
-        computerscoretext.innerHTML = computerscore;
+        computerScore += 1;
+        computerScoreText.innerHTML = computerScore;
         console.log("You lost... You chose rock and the computer chose paper");
         answer.innerHTML = "You lost... You chose rock and the computer chose paper";
     }
@@ -30,16 +30,16 @@ function playPaper(){
     let computerChoice = Math.floor(Math.random()*3);
     let answer = document.querySelector("#answer");
     if(computerChoice === 0){
-        playerscore += 1;
-        yourscoretext.innerHTML = playerscore;
+        playerScore += 1;
+        yourScoreText.innerHTML = playerScore;
         console.log("You won! You chose paper and the computer chose rock");
         answer.innerHTML = "You won! You chose paper and the computer chose rock";
     }else if(computerChoice === 1){
         console.log("It's a tie, you both chose paper");
         answer.innerHTML = "It's a tie, you both chose paper";
     }else if(computerChoice === 2){
-        computerscore += 1;
-        computerscoretext.innerHTML = computerscore;
+        computerScore += 1;
+        computerScoreText.innerHTML = computerScore;
         console.log("You lost... You chose paper and the computer chose scissors");
         answer.innerHTML = "You lost... You chose paper and the computer chose scissors";
     }
@@ -53,16 +53,16 @@ function playScissors(){
     let computerChoice = Math.floor(Math.random()*3);
     let answer = document.querySelector("#answer");
     if(computerChoice === 0){
-        playerscore += 1;
-        yourscoretext.innerHTML = playerscore;
+        playerScore += 1;
+        yourScoreText.innerHTML = playerScore;
         console.log("You won! You chose scissors and the computer chose paper");
         answer.innerHTML = "You won! You chose scissors and the computer chose paper";
     }else if(computerChoice === 1){
         console.log("It's a tie, you both chose rock");
         answer.innerHTML = "It's a tie, you both chose scissors";
     }else if(computerChoice === 2){
-        computerscore += 1;
-        computerscoretext.innerHTML = computerscore;
+        computerScore += 1;
+        computerScoreText.innerHTML = computerScore;
         console.log("You lost... You chose scissors and the computer chose rock");
         answer.innerHTML = "You lost... You chose scissors and the computer chose rock";
     }
